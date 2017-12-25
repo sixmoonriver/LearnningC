d6a7d675bca9e74b96a64302a45bb9e24f844967
yfy <yaofayang@qq.com>
2017-12-25 15:25:16 +0800

    fix bug

--- test-switch.c
+++ test-switch.c
@@ -7,8 +7,8 @@ int main()
     switch(no%3)
     {
         case 0: printf("这个数可以被3整除");break;
-        case 1: printf("这个数不可以被3整除，余数为:%d,no%3"); break;
-        case 2: printf("这个数不可以被3整除，余数为:%d,no%3"); break;
+        case 1: printf("这个数不可以被3整除，余数为:%d",no%3); break;
+        case 2: printf("这个数不可以被3整除，余数为:%d",no%3); break;
         default: printf("不可能！");
     }
 
